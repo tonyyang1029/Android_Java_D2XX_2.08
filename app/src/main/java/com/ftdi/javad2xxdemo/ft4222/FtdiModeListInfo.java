52d66297ff6e50468a4a5baa052575bb11d91e4b
tonyyang1029 <tonyyang1029@gmail.com>
2019-01-16 09:58:39 +0800

    Initial commit

---
+++ app/src/main/java/com/ftdi/javad2xxdemo/ft4222/FtdiModeListInfo.java
@@ -0,0 +1,17 @@
+package com.ftdi.javad2xxdemo.ft4222;
+
+public class FtdiModeListInfo {
+    static String[] TITLES = {
+        "Information",
+        "FT4222 - SPI",
+        "FT4222 - I2C",
+        "FT4222 - GPIO",
+    };
+
+    static String[] DIALOGUE = {
+        "Device Information",
+        "FT4222 SPI",
+        "FT4222 I2C",
+        "FT4222 GPIO",
+    };
+}
